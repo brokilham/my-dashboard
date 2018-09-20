9679b853560be48fa4f2cef1c584dcd4738638b3
brokilham <plato.ilham@gmail.com>
2018-09-21 00:24:22 +0700

    add form login

---
+++ resources/views/auth/layouts/auth.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('page')
+
+    {{--Auth Content--}}
+    @yield('content')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['namespace' => 'Auth'], function () {
+    Route::get('/login','LoginController@showLoginForm');
+});
